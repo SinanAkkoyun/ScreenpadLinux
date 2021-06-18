0e425db11f17714c47f24b51c1ee2dd1132a7c3d
SinanAkkoyun <akkoyunsinan2@gmx.de>
2021-06-18 17:09:43 +0200

    done

--- disable_screenpad.sh
+++ disable_screenpad.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-echo 0 | sudo tee '/sys/class/leds/asus::screenpad/brightness'
+echo 0 | tee '/sys/class/leds/asus::screenpad/brightness'
 xrandr --output HDMI-0 --off
 echo "false" > /opt/screenpad/screenpad_on
 notify-send 'Screenpad' 'Disabled Screenpad'
