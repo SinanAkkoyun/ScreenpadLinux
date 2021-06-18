46c269b971b480ba1fcd4b55c37ac2f64f2dd2bb
Sinan Akkoyun <43215895+SinanAkkoyun@users.noreply.github.com>
2021-06-18 02:37:10 +0000

    Update startup_screenpad_service.sh

--- startup_screenpad_service.sh
+++ startup_screenpad_service.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 sudo chmod a+w '/sys/class/leds/asus::screenpad/brightness'
+sudo touch /tmp/screenpad_brightness
+sudo chmod 777 /tmp/screenpad_brightness
 echo "255" > /tmp/screenpad_brightness
 
 # ADD CHOICE IN INSTALLER
 # isNVIDIA=$(optimus-manager --print-mode)
 # Current GPU mode : nvidia
-/opt/screenpad/enable_screenpad.sh
+# /opt/screenpad/enable_screenpad.sh
