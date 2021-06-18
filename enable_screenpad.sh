cb05e56862241c1c48731c9ff56ee3df36b1cd9f
SinanAkkoyun <akkoyunsinan2@gmx.de>
2021-06-19 01:38:42 +0200

    yay

--- enable_screenpad.sh
+++ enable_screenpad.sh
@@ -4,23 +4,28 @@
 screenpad_intel="HDMI-1-0"
 screenpad_nvidia="HDMI-0"
 
+screenpad_output=$screenpad_intel
+optimus_mode=$(optimus-manager --print-mode)
+
+if [[ "$optimus_mode" == *"hybrid"* ]]; then
+	echo "Intel"
+	screenpad_output=$screenpad_intel
+fi
+if [[ "$optimus_mode" == *"nvidia"* ]]; then
+	echo "NVIDIA"
+	screenpad_output=$screenpad_nvidia
+fi
+if [[ "$optimus_mode" == *"integrated"* ]]; then
+	echo "Integrated, on your model this will not work."
+	notify-send "Screenpad" "Screenpad not enabled, please switch to Hybrid or NVIDIA in Optimus Manager."
+	exit 1
+fi
+
 notify-send 'Screenpad' 'Enabling Screenpad...'
 /opt/screenpad/set_screenpad_brightness.sh 255
 
 # Adjust and shrink as you can
 sleep 5s
-
-screenpad_output=$screenpad_intel
-optimus_mode=$(optimus-manager --print-mode)
-
-if [[ "$optimus_mode" == *"hybrid"* || "$optimus_mode" == *"integrated"* ]]; then
-  echo "Intel"
-  screenpad_output=$screenpad_intel
-fi
-if [[ "$optimus_mode" == *"nvidia"* ]]; then
-  echo "NVIDIA"
-  screenpad_output=$screenpad_nvidia
-fi
 
 xrandr --newmode "screenpad" 140.00 1080 1150 1170 1195 1920 1924 1926 1930 -hsync -vsync
 xrandr --addmode "$screenpad_output" "screenpad"
